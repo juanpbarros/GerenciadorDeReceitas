13152a42bf9bdf2f71b2799f2dceb6a2d3503a7d
Juan Barros <juanp.barroos@gmail.com>
2026-06-11 16:29:52 -0300

    feat: preparar configuracao de deploy

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import cors from 'cors'
+import { corsOptions } from './config/cors.js'
 import routes from './routes/index.js'
 import { errorHandler, notFoundHandler } from './middlewares/errorHandler.js'
 
 const app = express()
 
-app.use(cors())
+app.use(cors(corsOptions()))
 app.use(express.json())
 
 app.use('/api', routes)
